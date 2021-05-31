7f0ad4789b7f7ad1861f7cdba944e6bbf2e3ed27
sangeetds <sangeetnarayands@gmail.com>
2021-05-31 17:43:20 +0530

    * Made model members private * Refactor * Optional handling

--- src/main/java/com/socialbox/model/Group.java
+++ src/main/java/com/socialbox/model/Group.java
@@ -1,22 +1,21 @@
 package com.socialbox.model;
 
+import java.util.HashSet;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
-import java.util.HashSet;
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Group {
-    @Id
-    public String groupId;
-    public String groupName;
-    public List<String> groupMovieList;
-    public HashSet<String> usersId;
+  @Id
+  public String groupId;
+  public String groupName;
+  public List<String> groupMovieList;
+  public HashSet<String> usersId;
 }
