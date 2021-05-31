22917b36f2c5ef723e591b7da3af6000a463c639
sangeetds <sangeetnarayands@gmail.com>
2021-05-31 17:49:54 +0530

    Import reordering.

--- src/main/java/com/socialbox/service/impl/UserServiceImpl.java
+++ src/main/java/com/socialbox/service/impl/UserServiceImpl.java
@@ -3,9 +3,10 @@ package com.socialbox.service.impl;
 import com.socialbox.model.User;
 import com.socialbox.repository.UserRepository;
 import com.socialbox.service.UserService;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
-import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,12 +22,8 @@ public class UserServiceImpl implements UserService {
   }
 
   public User getUserById(String id) {
-<<<<<<< HEAD
     Optional<User> userOptional = this.userRepository.findById(id);
     return userOptional.orElse(null);
-=======
-    return this.userRepository.findById(id).get();
->>>>>>> 9912b23a2462f2bc6e7e12a423ab067fdb78eaa2
   }
 
   public User saveUser(User user) {
