c3d211808a06828bc8287e0548d375e994c9c47e
sangeetds <sangeetnarayands@gmail.com>
2021-05-31 17:49:54 +0530

    Import reordering.

--- src/main/java/com/socialbox/model/MovieList.java
+++ src/main/java/com/socialbox/model/MovieList.java
@@ -1,11 +1,12 @@
 package com.socialbox.model;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
--- src/main/java/com/socialbox/model/User.java
+++ src/main/java/com/socialbox/model/User.java
@@ -1,12 +1,13 @@
 package com.socialbox.model;
 
-import java.util.HashSet;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+
+import java.util.HashSet;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
