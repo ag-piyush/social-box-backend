f5723b49a77791f6fbc4754f3457a23bbe2a1afb
Piyush <piyush.ag.bitsg@gmail.com>
2021-06-02 23:07:07 +0530

    Added MovieController and MovieService

---
+++ src/main/java/com/socialbox/controllers/MovieController.java
@@ -0,0 +1,36 @@
+package com.socialbox.controllers;
+
+import com.socialbox.dto.MovieDTO;
+import com.socialbox.model.Movie;
+import com.socialbox.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/movie")
+@RestController
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService){
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<MovieDTO> getAllMovies(){
+        return this.movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovie(@PathVariable("id") String id){
+        return this.movieService.getMovie(id);
+    }
+
+    @PostMapping
+    public Movie saveMovie(@RequestBody Movie movie){
+        return this.movieService.saveMovie(movie);
+    }
+}
