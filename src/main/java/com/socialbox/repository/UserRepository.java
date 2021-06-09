8e11d5546d538513fa6f53bb99c9bb7dfb61132a
sangeetds <sangeetnarayands@gmail.com>
2021-06-10 02:49:45 +0530

    * Added logs * Added mongoTemplate for better DB query

--- src/main/java/com/socialbox/repository/UserRepository.java
+++ src/main/java/com/socialbox/repository/UserRepository.java
@@ -1,8 +1,16 @@
 package com.socialbox.repository;
 
 import com.socialbox.model.User;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository {
     User findByUserEmail(String email);
+
+    List<User> findAll();
+
+    Optional<User> findById(String id);
+
+    User save(User user);
 }
--- src/main/java/com/socialbox/service/impl/UserServiceImpl.java
+++ src/main/java/com/socialbox/service/impl/UserServiceImpl.java
@@ -3,12 +3,14 @@ package com.socialbox.service.impl;
 import com.socialbox.model.User;
 import com.socialbox.repository.UserRepository;
 import com.socialbox.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
@@ -25,17 +27,25 @@ public class UserServiceImpl implements UserService {
   @Override
   public User getUserById(String id) {
     Optional<User> userOptional = this.userRepository.findById(id);
+    userOptional.ifPresent(user -> log.info("Found user: {}", user));
+    if (!userOptional.isPresent()) {
+      log.error("User not found with id: {}", id);
+    }
+
     return userOptional.orElse(null);
   }
 
   @Override
   public User saveUser(User user) {
-
     User existingUser = this.userRepository.findByUserEmail(user.getUserEmail());
+    log.info("User found: {}", existingUser);
 
-    if(existingUser != null && user.getUserPassword().equals(existingUser.getUserPassword()))
+    if(existingUser != null && user.getUserPassword().equals(existingUser.getUserPassword())) {
+      log.info("User authenticated.");
       this.userRepository.save(existingUser);
+    }
 
+    log.error("Login credentials incorrect.");
     return null;
   }
 }
