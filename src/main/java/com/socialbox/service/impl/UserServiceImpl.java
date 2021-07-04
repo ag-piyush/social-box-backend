583f1c996d1f8d04d751adc56e55c3d8cef54e88
Piyush <piyush.ag.bitsg@gmail.com>
2021-07-04 13:16:16 +0530

    Login user bug fix

--- src/main/java/com/socialbox/service/impl/UserServiceImpl.java
+++ src/main/java/com/socialbox/service/impl/UserServiceImpl.java
@@ -45,7 +45,10 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public User saveUser(User user) {
-    return this.userRepository.save(user);
+
+    Optional<User> userOptional = this.userRepository.findById(user.getId());
+
+    return userOptional.orElseGet(() -> this.userRepository.save(user));
   }
 
   @Override
