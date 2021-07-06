1f5fc78214d23cd1cc80985f7285b2fb042d9707
Piyush <piyush.ag.bitsg@gmail.com>
2021-07-07 03:56:22 +0530

    Chatbox initial

---
+++ src/main/java/com/socialbox/model/Chat.java
@@ -0,0 +1,15 @@
+package com.socialbox.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Chat {
+    public String content;
+    public String sender;
+}
